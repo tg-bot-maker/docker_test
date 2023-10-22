a91a2cecd1122924b21151094cd0b958db2de53f
tg-bot-maker <the_bot_maker@rambler.ru>
2023-10-22 15:33:46 +0300

    Added get_ip function

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ def opa():
     return "Hello buddy!!"
 
 
+@app.route("/get_ip")
+def get_ip():
+    return request.remote_addr
+    
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
